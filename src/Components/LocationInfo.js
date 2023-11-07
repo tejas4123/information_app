6fc3537d18e717f9f20c6a6b22fcbad46cb54814
tejas4123 <96643120+tejas4123@users.noreply.github.com>
2023-11-07 23:49:09 +0530

    styling

--- src/Components/LocationInfo.js
+++ src/Components/LocationInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from '../Card/Card';
 
 function LocationInfo({ location, error, loading, onClear }) {
   if (loading) {
@@ -12,30 +13,61 @@ function LocationInfo({ location, error, loading, onClear }) {
   if (!location) {
     return null;
   }
+
+  let loc = location["post code"];
+  let coun = location.country;
+  let abb = location["country abbreviation"];
   
   return (
-    <div>
-      <h2>Location Information</h2>
-      {location && (
-        <div>
-          <p>Post code : {location["post code"]}</p>
-          <p>Country: {location.country}</p>
-          <p>country abbreviation : {location["country abbreviation"]}</p>
-          <ul>
-              {location.places.map((place, placeIndex) => (
-                <li key={placeIndex}>
-                  <p>Place Name: {place["place name"]}</p>
-                  <p>Longitude: {place.longitude}</p>
-                  <p>state: {place.state}</p>
-                  <p>state Abbreviation: {place["state abbreviation"]}</p>
-                  <p>Latitude : {place["latitude"]}</p>
-                </li>
-              ))}
-            </ul>
+    // <Card>
+    //   <h2>Location Information</h2>
+    //   {location && (
+    //     <div>
+    //       <p>Post code : {location["post code"]}</p>
+    //       <p>Country: {location.country}</p>
+    //       <p>country abbreviation : {location["country abbreviation"]}</p>
+    //       <ul>
+    //           {location.places.map((place, placeIndex) => (
+    //             <li key={placeIndex}>
+    //               <p>Place Name: {place["place name"]}</p>
+    //               <p>Longitude: {place.longitude}</p>
+    //               <p>state: {place.state}</p>
+    //               <p>state Abbreviation: {place["state abbreviation"]}</p>
+    //               <p>Latitude : {place["latitude"]}</p>
+    //             </li>
+    //           ))}
+    //         </ul>
             
-        </div>
-      )}
-      <button onClick={onClear}>Clear</button>
+    //     </div>
+    //   )}
+    //   <button onClick={onClear}>Clear</button>
+    // </Card>
+
+    <div className="max-w-[1170px] mx-auto sm:p-7 p-4 text-white">
+      {/* information Section */}
+      <div>
+        <h3 className="font-semibold sm:text-xl">
+          Location According to PIncode:
+        </h3>
+      </div>
+      {/* location Card */}
+      <main className="grid lg:grid-cols-3 gap-6 py-8 ">
+        {location && location.places.map((place, placeIndex) => (
+         <div className="shadow-lg shadow-white rounded-lg p-5 bg-[#689850]">
+         <div className='text-center'>
+            <h2 className="font-poppins text-2xl font-semibold py-2">{coun}</h2>
+          </div>
+            <ul className='p-2'>
+              <li key={placeIndex}>
+              <h3> {place["place name"]}</h3>
+              <h3>{place.state}</h3>
+               
+
+              </li>
+             </ul>
+        </div>))}
+        {/* ))} */}
+      </main>
     </div>
   );
 }
--- src/Components/ZipCodeForm.js
+++ src/Components/ZipCodeForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function ZipCodeForm({ onZipCodeSubmit }) {
+function ZipCodeForm({ onZipCodeSubmit,  }) {
   const [zipCode, setZipCode] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -22,17 +22,31 @@ function ZipCodeForm({ onZipCodeSubmit }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Enter Zip Code"
-        value={zipCode}
-        onChange={(e) => setZipCode(e.target.value)}
-      />
-      <button type="submit">Submit</button>
-      {loading && <div>Loading...</div>}
-      {error && <div>Error: {error.message}</div>}
-    </form>
+    <div className="max-w-[1170px] mx-auto sm:py-[18vh] py-[18vh] text-center">
+        <h1 className="text-white sm:text-[60px] text-[35px] font-bold">
+          Find Your Location
+        </h1>
+        <h4 className="text-white text-[25px] sm:py-[20px] py-[10px]">
+          By Your Pincode
+        </h4>
+        {/* search section  */}
+        <div className="max-w-[700px] mx-auto sm:px-[10px] px-[40px] mt-[30px] text-white">
+          <form className="grid sm:grid-cols-[70%_auto] grid-cols-1 gap-[20px]" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter Your Pincode"
+              className="border-[1px]  pl-[20px] h-[50px] border-[#ccc] bg-[transparent]"
+              value={zipCode}
+              onChange={(e) => setZipCode(e.target.value)}
+            ></input>
+            <button type='submit' className="bg-[red] rounded-[5px] sm:p-[7px_20px] p-[5px_15px] py-[15px] ">
+              Find Out
+            </button>
+            {loading && <div>Loading...</div>}
+            {error && <div>Error: {error.message}</div>}
+          </form>
+        </div>
+      </div>
   );
 }
 
